b85beeffa281476956d315df2acf87450df2821b
José Ramón Ruiz <Jr-Ruiz@users.noreply.github.com>
2016-01-27 23:24:08 +0100

    Update aSlider.js

--- aSlider.js
+++ aSlider.js
@@ -1,38 +1,54 @@
-$(document).ready(function() {
-	$("#right").bind("click",right);
-	$("#left").bind("click",left);
-		
-	function right(){
-	
-		$("#right").unbind();
-		$("#left").unbind();
+$(document).ready(function(){
+	var image_width=$("#slider li img").width();
+  	var no_li=$("#slider ul li").size();
 
-		$("#slider ul").css("margin-left",-800);
+  	$("#slider, #slider ul li").css(
+  		"width", image_width
+  	);
+
+  	$("#slider ul").css(
+  		"width", image_width*no_li
+  	);
+
+	add_click();
+		
+	function move_right(){
+
+		remove_click();
+
+		$("#slider ul").css("margin-left",-image_width);
 		$("#slider ul li:last").prependTo("#slider ul");
 		$("#slider ul").animate({"margin-Left":0},1000,function(){
-			$("#right").bind("click",right);
-			$("#left").bind("click",left);
+			add_click();
 		});	
 
 	};
 
 
-	function left(){
+	function move_left(){
+	
+		remove_click();
 
-		$("#right").unbind();
-		$("#left").unbind();
-			
 		$("#slider ul").animate({
-				"margin-left" : "-=800px"
+			"margin-left" : "-="+image_width+"px"
 			}, 1000,function(){
 
 			$("#slider ul li:first").appendTo("#slider ul");
 			$("#slider ul").css("margin-left",0);
 
-			$("#right").bind("click",right);
-			$("#left").bind("click",left);
-		
+			add_click();
+				
 		});
 	};
 
+	function remove_click(){
+		$("#right").unbind();
+		$("#left").unbind();
+	}
+
+	function add_click(){
+		$("#right").bind("click", move_right);
+		$("#left").bind("click", move_left);
+	}
+		
 });
